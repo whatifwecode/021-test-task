e8e36984939b6dd320f38010ef608f528ec08825
Jegor <jegor.kuznetsov@gmail.com>
2022-07-04 23:11:15 +0300

    - description

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,29 +1,37 @@
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
+import React from "react";
+import Grid from "@material-ui/core/Grid";
 
-import Product from './Product/Product';
+import Product from "./Product/Product";
 
 const products = [
-    {id: 1, name: 'Shoes', desciption: 'Running shoes.', price: '$5', rating: '2.2'},
-    {id: 2, name: 'Cap', description: 'Yankee`s cap.', price: '$10', rating: '3.5'}
-]
+  {
+    id: 1,
+    name: "Shoes",
+    description: "Running shoes.",
+    price: "$5",
+    rating: "2.2",
+  },
+  {
+    id: 2,
+    name: "Cap",
+    description: "Yankee`s cap.",
+    price: "$10",
+    rating: "3.5",
+  },
+];
 
 const Products = () => {
-    return (
+  return (
     <main>
-        <Grid container justify='center' spacing={4}>
-            {
-                products.map((product) => (
-                    <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                        <Product product={product}/>
-                    </Grid>
-                ))
-            }
-        </Grid>
+      <Grid container justify="center" spacing={4}>
+        {products.map((product) => (
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} />
+          </Grid>
+        ))}
+      </Grid>
     </main>
-    );
-}
-
-
+  );
+};
 
 export default Products;
